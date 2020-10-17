25643d774d02cd84150084ee248aea55b180a77e
chenqiangjun <unityboby@gmail.com>
2020-10-17 18:47:06 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <div class="content">
+      <chatcom :currentUser="currentUser"></chatcom>
+      <userlistcom @chooseuser='toggleUser' :userlist="userlist"></userlistcom>
+    </div>
+  </div>
+</template>
+
+<script>
+  import chatcom from "./components/chatcom";
+  import userlistcom from "./components/userlistcom";
+
+  export default {
+    name: 'App',
+    components: {
+      chatcom,
+      userlistcom,
+    },
+    data() {
+      return {
+        msg: '聊天',
+        userlist: [
+          {
+            username: "小明",
+            headerimg: require('./assets/img/1.jpeg')
+          },
+          {
+            username: "小黑",
+            headerimg: require('./assets/img/2.jpeg')
+          },
+          {
+            username: "小红",
+            headerimg: require('./assets/img/3.jpg')
+          },
+          {
+            username: "小白",
+            headerimg: require('./assets/img/4.jpeg')
+          }
+        ],
+        currentUser:{
+          username:"小明",
+          headerimg:require('./assets/img/1.jpeg')
+        }
+      }
+    },
+    methods: {
+      toggleUser:function(index) {
+        this.currentUser = this.userlist[index]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .content {
+    display: flex;
+    width: 800px;
+    height: 700px;
+    margin: 100px auto;
+  }
+</style>
---
+++ src/components/chatcom.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="chat">
+    <h1 class="user">用户：{{currentUser.username}}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['currentUser']
+  }
+</script>
+
+<style scoped>
+  .chat{
+    width: 500px;
+    height: 700px;
+    border: 1px solid #ccc;
+  }
+</style>
